b2de72eb5ac0a2b4019b3f6a3a31f2284ec76f67
Aarti Rao <amanjesh@asu.edu>
2016-10-23 11:09:11 -0700

    Edited transcode script

--- transcode.sh
+++ transcode.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-path="/usr/local/ngnix/Videos/vod2/"
+echo $1 
+echo $2
+echo "----------Starting encoding-------"
+
+path="/usr/local/nginx/Videos/vod/"
 filename=$1
 input_file=$path$1
 output_file="$2.mp4"
 output_filename=$path$output_file
 
 input_name=${filename%.*}
-#input_name="sample"
 inter="intermediate_${input_name}_2400k.264"
 
 x264 --output $inter --fps 24 --preset slow --bitrate 2400 --vbv-maxrate 4800 --vbv-bufsize 9600 --min-keyint 48 --keyint 48 --scenecut 0 --no-scenecut --pass 1 --video-filter "resize:width=1280,height=720" $input_file
