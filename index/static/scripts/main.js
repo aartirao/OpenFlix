d2ab23213acf000886f43bad73c3445e8190ca5a
Aarti Rao <amanjesh@asu.edu>
2016-10-30 23:42:58 -0700

    added URL access for videos

--- index/static/scripts/main.js
+++ index/static/scripts/main.js
@@ -1,14 +1,24 @@
+var getUrlParameter = function getUrlParameter(sParam) { var sPageURL = decodeURIComponent(window.location.search.substring(1)), sURLVariables = sPageURL.split('&'), sParameterName, i; for (i = 0; i < sURLVariables.length; i++) { sParameterName = sURLVariables[i].split('='); if (sParameterName[0] === sParam) { return sParameterName[1] === undefined ? true : sParameterName[1]; } } };
+
+function play_video(filename) {
+	player = dashjs.MediaPlayer().create();
+	player.initialize($("#player").get(0), "http://10.1.20.113/dash_vod/"+filename+"_dash.mpd", true);
+	$("#player").get(0).load();
+	$("#player").get(0).play();
+
+}
+
 $(document).ready(function () {
+	var fname = getUrlParameter("id");
+	play_video(fname);
+
   $(".img-class").click(function(){ 
 	$("#player").get(0).pause();	
 	var filename = this.id;
 	filename = filename.replace('static/images/thumbnails/','');
-	filename = filename.replace('.jpeg','_dash.mpd');
-	player = dashjs.MediaPlayer().create();
-	player.initialize($("#player").get(0), "http://10.1.20.113/dash_vod/"+filename, true);
-	$("#player").get(0).load();
-	$("#player").get(0).play();
- });
+	filename = filename.replace('.jpeg','');
+	play_video(filename);
+	 });
  
  //$("#popup").dialog({ autoOpen: false, show: 'slide', resizable: false, position: 'center', stack: true, height: 'auto', width: 'auto', modal: true });
  //$("#popup").dialog('open'); 
